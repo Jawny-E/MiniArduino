c7e1db82580357ebb52ae26c225a77490a60a6e1
Ellen Johnsen <94108006+Jawny-E@users.noreply.github.com>
2023-10-19 18:10:23 +0200

    Update ElectronicDice.ino

--- ElectronicDice/ElectronicDice.ino
+++ ElectronicDice/ElectronicDice.ino
@@ -2,6 +2,12 @@
 int seg1Pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 0,}; 
 int seg2Pins[] = {10, 11, 12, 13, A0, A1, A2, A3};
 
+int button = A5;
+int potMeter = A6;
+
+//Generic global variables used to store different digits
+int d1, d2, num;
+
 //Truth table for the display, for common Cathode 7-segment
 byte segCode[11][8] = {
   //  a  b  c  d  e  f  g  .
@@ -17,8 +23,6 @@ byte segCode[11][8] = {
   { 0, 0, 0, 0, 1, 0, 0, 1},  // 9
   { 1, 1, 1, 1, 1, 1, 1, 1}   // BLANK
 };
-//Generic global variables used to store different digits
-int d1, d2, num;
 
 /*
   This function does the actual work of setting the pins
@@ -40,52 +44,60 @@ void displayDigit(int digit, int Pins[]){
 */
 void selectDigit(int dice){
     switch(dice){
-    case 6:
+    case 0:
       d1 = 10;
       d2 = random(1,7);
       //Serial.println(dice);
       //Serial.print("Resulatet er: " + String(d1) +' '+ String(d2) + '\n');
-      displayDigit(d1, seg1Pins);
-      displayDigit(d2, seg2Pins);
-      delay(100);
       displayDigit(10, seg1Pins);
       displayDigit(10, seg2Pins);
-      delay(100);
+      delay(400);
+      displayDigit(d1, seg1Pins);
+      displayDigit(d2, seg2Pins);
+      delay(300);
+      displayDigit(10, seg1Pins);
+      displayDigit(10, seg2Pins);
+      delay(300);
       displayDigit(d1, seg1Pins);
       displayDigit(d2, seg2Pins);
       delay(2000);
       dice = 20;
       break;
-    case 20:
+    case 1:
       num = random(1, 21);
       d1 = num / 10;
       d2 = num % 10;
       //Serial.println(dice);
       //Serial.print("Resulatet er: " + String(d1) +' '+ String(d2) + '\n');
-      displayDigit(d1, seg1Pins);
-      displayDigit(d2, seg2Pins);
-      delay(100);
       displayDigit(10, seg1Pins);
       displayDigit(10, seg2Pins);
-      delay(100);
+      delay(300);
+      displayDigit(d1, seg1Pins);
+      displayDigit(d2, seg2Pins);
+      delay(300);
+      displayDigit(10, seg1Pins);
+      displayDigit(10, seg2Pins);
+      delay(300);
       displayDigit(d1, seg1Pins);
       displayDigit(d2, seg2Pins);
       delay(2000);
       break;
-    case 100:
+    case 2:
       num = random(0, 100);
-      num = 99;
       d1 = num / 10;
       d2 = num % 10;
       //Serial.println(num);
       //Serial.println(dice);
       //Serial.print("Resulatet er: " + String(d1) +' '+ String(d2) + '\n');
-      displayDigit(d1, seg1Pins);
-      displayDigit(d2, seg2Pins);
-      delay(100);
       displayDigit(10, seg1Pins);
       displayDigit(10, seg2Pins);
-      delay(100);
+      delay(300);
+      displayDigit(d1, seg1Pins);
+      displayDigit(d2, seg2Pins);
+      delay(300);
+      displayDigit(10, seg1Pins);
+      displayDigit(10, seg2Pins);
+      delay(300);
       displayDigit(d1, seg1Pins);
       displayDigit(d2, seg2Pins);
       delay(2000);
@@ -94,46 +106,45 @@ void selectDigit(int dice){
 
   
 }
-
-void setup(){
-  //Serial.begin(9600);
-  for (int i = 0; i < 8; i++)
-  {
-    pinMode(seg1Pins[i], OUTPUT);
-    pinMode(seg2Pins[i], OUTPUT);
-  }
-  randomSeed(analogRead(A7));
-
-}
 /*
   Runs a random sequence of numbers on the 7-segment display
   The intent is for this to "mimic" the feeling of watching a rolling dice
 */
 void randomSequence(){
-  for (int i = 0; i < random(4, 10); i += 1) {
+  for (int i = 0; i < random(10, 20); i += 1) {
       d1 = random(10);
       d2 = random(10);
-      delay(100-(i*9));
+      delay(50+(20*i));
       displayDigit(d1, seg1Pins); 
       displayDigit(d2, seg2Pins);
+  }
+}
+/*
+  This function is responsible for choosing which dice to
+  roll dependent on the state of the potentiometer
+*/
+int potentiMeter(){
+  int meas = map(analogRead(potMeter), 0, 1023, 0, 3);
+  return meas;
+}
+
+void setup(){
+  Serial.begin(9600);
+
+  pinMode(button, INPUT);
+  pinMode(potMeter, INPUT);
+
+  for (int i = 0; i < 8; i++)
+  {
+    pinMode(seg1Pins[i], OUTPUT);
+    pinMode(seg2Pins[i], OUTPUT);
   }
 }
 
 void loop()
 {
-  /*Pseudocode since I need to Check the pinout of the nano*/
-  /*
-  if(button1 == HIGH){
+  if(digitalRead(button) == HIGH){
     randomSequence();
-    selectDigit(6);
+    selectDigit(potentiMeter());
   }
-  elif(button2 == HIGH){
-    randomSequence();
-    selectDigit(20);
-  }
-  elif(button 3 == HIGH){
-    randomSequence();
-    selectDigit(100);
-  }
-  */
 }
